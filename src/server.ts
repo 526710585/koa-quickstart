1f8d939929b3384eef557ad1be105d569c142daf
危唯 <526710585@qq.co>
2020-08-05 16:03:49 +0800

    1.初始化

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import Koa from 'koa';
+import cors from '@koa/cors';
+import bodyParser from 'koa-bodyparser';
+
+import {logger} from './logger'
+
+// 初始化 Koa 应用实例
+const app = new Koa();
+
+// 注册中间件
+app.use(cors());
+app.use(logger());
+app.use(bodyParser());
+
+// 响应用户请求
+app.use((ctx) => {
+  ctx.body = 'Hello Koa';
+});
+
+// 运行服务器
+app.listen(3000);
